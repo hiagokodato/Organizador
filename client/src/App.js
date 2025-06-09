8ba563be68093f13f4091d668d2837946675f1ba
Hiago Kodato <hiagokodato@gmail.com>
2025-06-09 16:00:53 -0300

    remap server application fix 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// Remove 'BrowserRouter as Router' da importação, pois não é mais usado aqui
+import { Routes, Route, Navigate } from 'react-router-dom'; 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
@@ -7,18 +8,18 @@ function App() {
   const token = localStorage.getItem('token');
 
   return (
-    <Router>
-      <div className="bg-gray-100 min-h-screen">
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route 
-            path="/" 
-            element={token ? <DashboardPage /> : <Navigate to="/login" />} 
-          />
-        </Routes>
-      </div>
-    </Router>
+    // O <Router> foi removido daqui
+    <div className="bg-gray-100 min-h-screen">
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route 
+          path="/" 
+          element={token ? <DashboardPage /> : <Navigate to="/login" />} 
+        />
+      </Routes>
+    </div>
+    // E daqui
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Importando o CSS com Tailwind
+import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'; // <-- 1. ADICIONE ESTA LINHA
 
-// Cria a raiz da aplicação na div com id 'root' do seu index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Renderiza o componente principal <App /> dentro da raiz
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* <-- 2. ADICIONE ESTA TAG DE ABERTURA */}
+      <App />
+    </BrowserRouter> {/* <-- 3. E ESTA TAG DE FECHAMENTO */}
   </React.StrictMode>
 );
